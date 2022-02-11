3fae472edb3ceec98d39404d03e6c63309caf517
higashitani0624 <higashitaniyu@crunchtimer.jp>
2022-02-11 17:43:32 +0900

    first commit portfolio

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="dist/style.css">
+<title>Sass 読み込みサンプル</title>
+</head>
+<body>
+  <?php get_header(); ?>
+<script src="dist/main.js"></script><!-- main.js を読み込む -->
+</body>
+</html>
---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+//foundation.scss
+import './../scss/foundation/base.scss';
+import './../scss/foundation/mixin.scss';
+import './../scss/foundation/reset.scss';
+import './../scss/foundation/variable.scss';
+//layout.scss
+import './../scss/layout/l-footer.scss';
+import './../scss/layout/l-header.scss';
+import './../scss/layout/l-main.scss';
+//object.scss
+import './../scss/object/component/c-button.scss';
+import './../scss/object/project/p-about.scss';
+import './../scss/object/project/p-home.scss';
+import './../scss/object/project/p-contact.scss';
+import './../scss/object/project/p-skill.scss';
+import './../scss/object/utility/u-utility.scss';
+
+console.log('welcome to webpack');
