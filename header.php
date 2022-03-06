ea43c5d730e5646d5ae0a56ecdcefd47d1133c08
higashitani0624 <higashitaniyu@crunchtimer.jp>
2022-03-06 14:17:34 +0900

    google_tag_manager_add_20220306

--- header.php
+++ header.php
@@ -10,6 +10,13 @@
 
         gtag('config', 'G-43PBYV1V3Z');
     </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MBRCVZ9');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:url" content="<?php echo esc_url(home_url()); ?>" />
@@ -26,6 +33,10 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBRCVZ9"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <header>
         <div class="header">
             <div class="header-title">
